db402870b10ee62b3c7e2b5eb83a1e74e6e251ea
Liu Jingchu <liujingchu@gmail.com>
2014-09-26 22:42:23 +0800

    Revert "0902"
    This reverts commit 712e3107102c2c5a3974288307bdcac8348acd2f.

--- clusterFun.m
+++
@@ -1,5 +0,0 @@
-function [clustering] = clusterFun(nodeName,nodeComp,Adj,seed)
-    clustering = zeros(size(seed));
-    clustering = [ones(4,1);ones(4,1);2*ones(4,1);2*ones(2,1)];
-end
-
--- delay.m
+++
@@ -1,4 +0,0 @@
-function [ d ] = delay( nodeName,nodeComp,Adj,seed,clusters,params )
-    d = 0;
-end
-
